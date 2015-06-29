c1786d9b8c2072e6505192398dbe3eb2f6d13854
Steven Abrams <steven.abrams86@gmail.com>
2015-06-29 13:02:51 -0600

    updated home page index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../connection');
+var quizzes = db.get('quizzes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  quizzes.find({}, {}, function(err, docs){
+    res.render('index', {quizzes: docs});    
+  });
 });
 
 module.exports = router;
