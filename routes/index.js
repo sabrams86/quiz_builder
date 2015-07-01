c6783a10501e229b14543f8d25541b5d175c25cd
Steven Abrams <steven.abrams86@gmail.com>
2015-06-30 23:16:37 -0600

    users can search by user email

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,41 @@ var express = require('express');
 var router = express.Router();
 var db = require('./../connection');
 var quizzes = db.get('quizzes');
+var users = db.get('users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var userId = req.cookies.user_id;
   var categories = req.query.categorysearch;
-  var users = req.query.usersearch;
+  var userEmail = req.query.usersearch;
   var searchParams;
-  if (categories){
+  if (userEmail && categories) {
+    users.find({email: userEmail}, {}, function(err, userDocs){
+      searchParams = {$and: [{user_id: String(userDocs[0]._id)}, {categories : categories.toLowerCase()}]};
+      quizzes.find(searchParams, {}, function(err, docs){
+        res.render('index', {quizzes: docs, user_id: userId});
+      });
+    });
+  }
+  else if (categories){
     searchParams = {categories : categories.toLowerCase()};
-    if (users) {
-      searchParams = {categories: categories, email: users};
-    }
-  } else if (users) {
-    searchParams = {email: users};
+    quizzes.find(searchParams, {}, function(err, docs){
+      res.render('index', {quizzes: docs, user_id: userId});
+    });
+  } else if (userEmail) {
+    users.find({email: userEmail}, {}, function(err, userDocs){
+      searchParams = {user_id: String(userDocs[0]._id)};
+      quizzes.find(searchParams, {}, function(err, docs){
+        res.render('index', {quizzes: docs, user_id: userId});
+      });
+    });
   } else {
     searchParams = {};
+    quizzes.find(searchParams, {}, function(err, docs){
+      res.render('index', {quizzes: docs, user_id: userId});
+    });
   }
-  quizzes.find(searchParams, {}, function(err, docs){
-    res.render('index', {quizzes: docs, user_id: userId});
-  });
+
 });
 
 module.exports = router;
