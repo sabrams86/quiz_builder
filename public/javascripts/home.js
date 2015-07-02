3ae08c0355a8c740196ca8624457ed71681fe2d8
Steven Abrams <steven.abrams86@gmail.com>
2015-07-02 14:34:21 -0600

    added toggle class for quiz scores on home page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
 
   $('.quiz').on('click', 'button', function(){
     $(this).children().toggleClass('hidden');
-
+    $(this).parents('.quiz').children('.info').children('.quizdesc').toggleClass('hidden');
   });
-  // $('.score-button').on('click', 'button', function(){
-  //   $(this).children().toggleClass('hidden');
-  // });
+
 });
